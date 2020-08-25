9782616b96d20dd2d769fd2050e3622f3d1fbd17
Rajat Galav <rajat.galav@aurigait.com>
2020-08-25 16:57:33 +0530

    Updates

---
+++ precache-manifest.1ae2ecb964b34bb819cf0193290d98ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "953d22e3842fd1a4d00ca283c3c882c0",
+    "url": "/draw-canvas/index.html"
+  },
+  {
+    "revision": "d2aa6ac5c75e20596cf9",
+    "url": "/draw-canvas/static/css/main.7633b595.chunk.css"
+  },
+  {
+    "revision": "79297b4a485c3697e436",
+    "url": "/draw-canvas/static/js/2.c0c96937.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/draw-canvas/static/js/2.c0c96937.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2aa6ac5c75e20596cf9",
+    "url": "/draw-canvas/static/js/main.4cbed46b.chunk.js"
+  },
+  {
+    "revision": "6dad7df5f7a716e4ecec",
+    "url": "/draw-canvas/static/js/runtime-main.67dfe2c2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/draw-canvas/static/media/logo.5d5d9eef.svg"
+  }
+]);
